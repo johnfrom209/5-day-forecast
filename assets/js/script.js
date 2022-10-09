fbfaef7d95cb9e092f34e3c903413ec5b784330d
Jonathan Moreno <johnfrom209@gmail.com>
2022-10-08 18:05:42 -0700

    added moment to display the date

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,10 @@ var cityNameEl = $("#cityTitle");
 var cityTempEl = $("#cityTemp");
 var cityWindEl = $("#cityWind");
 var cityHumidEl = $("#cityHumidity");
+// var dailyDateEl = $("#dailyDate");
+var dayIcon = $("#dailyIcon");
+
+var displayTime = moment().format("L");
 
 var lat;
 var lon;
@@ -13,8 +17,7 @@ function fetchResults(event) {
     searchInputEl = $("#searchInput");
     var apiCall = 'http://api.openweathermap.org/geo/1.0/direct?q='
         + searchInputEl.val() + '&appid=6bd526cc976e4af732965c42955fed55'
-    console.log(apiCall);
-    console.log(searchInputEl.val());
+
     if (!searchInputEl.val()) {
         alert("Please enter a city");
         return;
@@ -42,12 +45,72 @@ function fetchResults(event) {
                 .then(function (data) {
                     console.log(data);
 
+                    var currentDate = $("<span>", { "class": "card-title" })
+                    currentDate.text(" (" + displayTime + ") ");
                     cityNameEl.text(data.city.name);
+
+                    // cityNameEl.append(currentDate);
+                    var iconCode = data.list[0].weather[0].icon;
+                    var dailyUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
+
+                    var dayIcon = $("<img>", { "src": dailyUrl });
+                    dayIcon.attr("lt", "Weather Icon");
+
+                    cityNameEl.append(currentDate).append(dayIcon);
                     cityTempEl.text(data.list[0].main.temp + " F");
                     cityWindEl.text(data.list[0].wind.speed + " MPH");
                     cityHumidEl.text(data.list[0].main.humidity + " %");
 
                     //next need to grab the next 5 days
+                    //api offers data sets every 3 hour
+                    //every 9th is 24h, so index 8
+
+                    $("#forecastText").text("5 Day Forecast");
+
+                    for (var i = 0; i < 5; i++) {
+                        var index = 8;
+
+                        //grab the settings and create the elements
+                        var daysEl = $("<div>", { "class": "card bg-dark text-white col-2 d-inline-block" });
+
+                        var daysBody = $("<div>", { "class": "card-body" });
+                        // var daysDate = $("<h5>", { "class": "card-title"})
+
+                        //in the process of trying to display the next 5 days
+                        var daysImage = $("<h6>", { "class": "card-subtitle mb-2" });
+
+                        //grab code from data
+                        // use moment to display the date
+                        displayTime = moment().add(i + 1, 'd');
+                        var forecastDateEl = $("<h5>", { "class:": "card-title" });
+                        forecastDateEl.text(displayTime);
+
+                        //grab the icon from data
+                        var daysIconCode = data.list[index].weather[0].icon;
+                        var daysIconUrl = "http://openweathermap.org/img/w/" + daysIconCode + ".png";
+                        var daysIcon = $("<img>", { "src": daysIconUrl });
+                        daysIcon.attr("alt", "Weather icon");
+
+                        // create h6 and display the temp
+                        var daysTemp = $("<h6>", { "class": "card-subtitle mb-2" });
+                        daysTemp.text(data.list[index].main.temp + " F");
+                        // display the wind speed
+                        var daysWind = $("<h6>", { "class": "card-subtitle mb-2" });
+                        daysWind.text(data.list[index].wind.speed + " MPH");
+                        // display the humidity
+                        var daysHumid = $("<h6>", { "class": "card-subtitle mb-2" });
+                        daysHumid.text(data.list[index].main.humidity + " %");
+
+                        // append that a card
+                        daysBody.append(forecastDateEl, daysIcon, daysTemp, daysWind, daysHumid)
+                        // append card to daysEl
+                        daysEl.append(daysBody);
+                        $(".daysForecast").append(daysEl);
+                        index += 8;
+
+                        //increment time for moment
+
+                    }
 
                 });
         });
